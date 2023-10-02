415300fe0402f94e422a2106d5ee890892580613
jessie-yook <sprout.jessie@gmail.com>
2023-10-02 21:15:01 +0900

    set styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles.styles';
 import Gnb from './components/Gnb';
 
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <Gnb />
       <h1>myBudget</h1>
     </div>
